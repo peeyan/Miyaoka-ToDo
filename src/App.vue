981ab5bb22eca9f98d04cd7a412bfd53eaef7e0e
github user name <github@example.com>
2024-08-31 12:23:30 +0900

    commit01

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrap">
+    <TheHeader />
+    <main class="main">メイン</main>
+    <TheFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheHeader from 'src/components/TheHeader.vue'
+import TheFooter from 'src/components/TheFooter.vue'
+</script>
+
+<style scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 90vh;
+  background-color: aqua;
+}
+
+.main {
+  flex: 1;
+  padding: 15px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="title">Miyaoka-Family ToDo</h1>
+</template>
+
+<style scoped>
+.title {
+  width: 100%;
+  padding: 16px 0;
+  text-align: center;
+  background-color: #fff;
+}
+</style>
